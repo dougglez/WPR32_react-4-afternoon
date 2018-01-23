e01b016b7c1fe45aaebc3cbfc3009acc6c8bcd06
dougglez <maxfield.douglas@gmail.com>
2018-01-23 14:05:44 -0700

    new project

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="box">
+        <button className='btn'>Math 1010</button>
+        <button className='btn'>English 2010</button>
+        <button className='btn'>Biology 2020</button>
+      </div>
+    );
+  }
+}
